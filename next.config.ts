3921e7906ce28ba320c871a4627e8c5d01ed8fa4
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-06-15 18:34:00 -0500

    feat: install mapbox package to use in app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    loadPaths: [path.join(process.cwd(), "src/shared/styles")],
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "i.pravatar.cc" },
+    ],
+  },
 };
 
 export default nextConfig;
